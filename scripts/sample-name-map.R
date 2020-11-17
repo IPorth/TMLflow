16302d8366a5abafb9014975c07740272f174ccf
IPorth <isabel.porth@medma.uni-heidelberg.de>
2020-11-17 13:22:46 +0100

    Create sample-name-map.R

---
+++ scripts/sample-name-map.R
@@ -0,0 +1,7 @@
+#each element is saved as list element
+samples <- snakemake@input[["sample"]]
+name <- snakemake@params[["name"]]
+outdir <- snakemake@output[[1]]
+map_df <- do.call(rbind, Map(data.frame, A=name, B=samples))
+
+write.table(map_df, outdir, quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
